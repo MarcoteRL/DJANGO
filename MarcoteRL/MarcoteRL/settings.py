ed63bcb851e2601316b931a884596628bece52d3
Marcote <andresmarcote13@gmail.com>
2023-02-08 16:51:05 +0100

    Update

--- MarcoteRL/MarcoteRL/settings.py
+++ MarcoteRL/MarcoteRL/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
